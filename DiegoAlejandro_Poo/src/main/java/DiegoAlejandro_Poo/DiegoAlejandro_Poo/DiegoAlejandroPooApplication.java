046b8c34037607911082823d39933a43701dede3
AAlejandrodd <20240657@ricaldone.edu.sv>
2025-10-01 16:16:57 -0600

    ya pa que :(

--- DiegoAlejandro_Poo/src/main/java/DiegoAlejandro_Poo/DiegoAlejandro_Poo/DiegoAlejandroPooApplication.java
+++ DiegoAlejandro_Poo/src/main/java/DiegoAlejandro_Poo/DiegoAlejandro_Poo/DiegoAlejandroPooApplication.java
@@ -1,5 +1,6 @@
 package DiegoAlejandro_Poo.DiegoAlejandro_Poo;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DiegoAlejandroPooApplication {
 
 	public static void main(String[] args) {
+		//Codigo para cargar los valores del archivo .env sobre el archivo application.properties
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry ->
+				System.setProperty(entry.getKey(), entry.getValue())
+		);
+
+		//Esta linea no se borra
 		SpringApplication.run(DiegoAlejandroPooApplication.class, args);
 	}
 
