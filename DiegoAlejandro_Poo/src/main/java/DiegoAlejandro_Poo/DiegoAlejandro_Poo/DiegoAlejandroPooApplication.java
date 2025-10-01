86314e13d79e742c649c73c5066c9b94ab8a47d6
AAlejandrodd <20240657@ricaldone.edu.sv>
2025-10-01 13:54:53 -0600

    dfg

---
+++ DiegoAlejandro_Poo/src/main/java/DiegoAlejandro_Poo/DiegoAlejandro_Poo/DiegoAlejandroPooApplication.java
@@ -0,0 +1,13 @@
+package DiegoAlejandro_Poo.DiegoAlejandro_Poo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiegoAlejandroPooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiegoAlejandroPooApplication.class, args);
+	}
+
+}
---
+++ DiegoAlejandro_Poo/src/test/java/DiegoAlejandro_Poo/DiegoAlejandro_Poo/DiegoAlejandroPooApplicationTests.java
@@ -0,0 +1,13 @@
+package DiegoAlejandro_Poo.DiegoAlejandro_Poo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiegoAlejandroPooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
